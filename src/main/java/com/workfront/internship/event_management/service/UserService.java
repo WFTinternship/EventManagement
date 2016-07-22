5397d53036bcb58ce238971e617de1ca3bcfd3f9
“Hermine Turshujyan” <hermineturshujyan@workfront.com>
2016-07-22 18:16:13 +0400

    Business logic layer started. UserService in progress

---
+++ src/main/java/com/workfront/internship/event_management/service/UserService.java
@@ -0,0 +1,32 @@
+package com.workfront.internship.event_management.service;
+
+import com.workfront.internship.event_management.exception.OperationFailedException;
+import com.workfront.internship.event_management.model.User;
+
+import java.util.List;
+
+/**
+ * Created by Hermine Turshujyan 7/21/16.
+ */
+interface UserService {
+
+    //CRUD operations with user
+    boolean addAccount(User user) throws OperationFailedException;
+
+    boolean editProfile(User user) throws OperationFailedException;
+
+    boolean verifyAccount(int userId) throws OperationFailedException;
+
+    User login(String email, String password) throws OperationFailedException;
+
+    boolean deleteAccount(int userId) throws OperationFailedException;
+
+    User getUserById(int userId) throws OperationFailedException;
+
+    User getUserByEmail(String email) throws OperationFailedException;
+
+    //operations with all users
+    List<User> getAllUsers() throws OperationFailedException;
+
+    boolean deleteAllUsers() throws OperationFailedException;
+}
---
+++ src/main/java/com/workfront/internship/event_management/service/UserServiceImpl.java
@@ -0,0 +1,194 @@
+package com.workfront.internship.event_management.service;
+
+import com.workfront.internship.event_management.dao.UserDAO;
+import com.workfront.internship.event_management.dao.UserDAOImpl;
+import com.workfront.internship.event_management.exception.DataAccessException;
+import com.workfront.internship.event_management.exception.DuplicateEntryException;
+import com.workfront.internship.event_management.exception.OperationFailedException;
+import com.workfront.internship.event_management.model.User;
+import com.workfront.internship.event_management.service.util.HashGeneratorUtil;
+
+import java.util.List;
+
+/**
+ * Created by Hermine Turshujyan 7/20/16.
+ */
+public class UserServiceImpl implements UserService {
+
+    private UserDAO userDAO;
+
+    public UserServiceImpl() throws OperationFailedException {
+        userDAO = new UserDAOImpl();
+    }
+
+    @Override
+    public boolean addAccount(User user) throws OperationFailedException {
+
+        boolean success = false;
+
+        //check if user object is valid
+        if (isValid(user)) {
+
+            //encrypt user password
+            String encryptedPassword = HashGeneratorUtil.generateHashString(user.getPassword());
+            user.setPassword(encryptedPassword);
+
+            try {
+                //check if user with this email already exists
+                User existingUser = userDAO.getUserByEmail(user.getEmail());
+
+                if (existingUser == null) {
+                    int userId = userDAO.addUser(user);
+                    if (userId != 0) {
+                        success = true;
+                    }
+                } else {
+                    throw new OperationFailedException("User with this email already exists!");
+                }
+            } catch (DataAccessException e) {
+                throw new OperationFailedException("Database error!");
+            }
+        }
+        return success;
+    }
+
+    @Override
+    public boolean editProfile(User user) throws OperationFailedException {
+
+        boolean success = false;
+
+        if (isValid(user)) {
+            try {
+                success = userDAO.updateUser(user);
+               /* if(!success) {
+                    throw new OperationFailedException("Non existing user.");
+                }*/
+            } catch (DuplicateEntryException e) {
+                throw new OperationFailedException("User with this email already exists!");
+            } catch (DataAccessException e) {
+                throw new OperationFailedException("Database error!");
+            }
+        }
+
+        return success;
+    }
+
+    @Override
+    public boolean verifyAccount(int userId) throws OperationFailedException {
+
+        boolean success = false;
+
+        if (userId > 0) {
+            try {
+                success = userDAO.updateVerifiedStatus(userId);
+              /*  if(!success) {
+                    throw new OperationFailedException("Non existing user.");
+                }*/
+            } catch (DataAccessException e) {
+                throw new OperationFailedException("Database error!");
+            }
+        }
+        return success;
+    }
+
+    @Override
+    public boolean deleteAccount(int userId) throws OperationFailedException {
+
+        boolean success = false;
+        if (userId > 0) {
+            try {
+                success = userDAO.deleteUser(userId);
+            /*  if(!success) {
+                    throw new OperationFailedException("Non existing user.");
+                }*/
+            } catch (DataAccessException e) {
+                throw new OperationFailedException("Database error!");
+            }
+        }
+        return success;
+    }
+
+    @Override
+    public User login(String email, String password) throws OperationFailedException {
+
+        User validUser = null;
+
+        try {
+            User user = userDAO.getUserByEmail(email);
+            if (user != null && user.getPassword().equals(HashGeneratorUtil.generateHashString(password))) {
+                validUser = user;
+            }
+        } catch (DataAccessException e) {
+            throw new OperationFailedException("Database error!");
+        }
+
+        return validUser;
+    }
+
+    @Override
+    public User getUserById(int userId) throws OperationFailedException {
+
+        User user;
+        try {
+            user = userDAO.getUserById(userId);
+        } catch (DataAccessException e) {
+            throw new OperationFailedException("Database error!");
+        }
+
+        return user;
+    }
+
+    @Override
+    public User getUserByEmail(String email) throws OperationFailedException {
+
+        User user;
+
+        try {
+            user = userDAO.getUserByEmail(email);
+        } catch (DataAccessException e) {
+            throw new OperationFailedException("Database error!");
+        }
+        return user;
+    }
+
+    @Override
+    public List<User> getAllUsers() throws OperationFailedException {
+
+        List<User> userList;
+
+        try {
+            userList = userDAO.getAllUsers();
+        } catch (OperationFailedException e) {
+            throw new OperationFailedException("Database error!");
+        }
+        return userList;
+    }
+
+    @Override
+    public boolean deleteAllUsers() throws OperationFailedException {
+
+        boolean success;
+
+        try {
+            success = userDAO.deleteAllUsers();
+        } catch (DataAccessException e) {
+            throw new OperationFailedException("Database error!");
+        }
+        return success;
+    }
+
+
+    //helper methods
+    private boolean isValid(User user) {
+
+        boolean valid = false;
+
+        if (user != null) {
+            if (user.getFirstName() != null && user.getLastName() != null
+                    && user.getPassword() != null && user.getEmail() != null) {
+                valid = true;
+            }
+        }
+        return valid;
+    }
+}
