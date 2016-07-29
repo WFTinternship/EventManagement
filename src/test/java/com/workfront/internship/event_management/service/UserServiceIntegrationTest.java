d13869f53315462c3a9ca14400f89cc5bca1e7ed
“Hermine Turshujyan” <hermineturshujyan@workfront.com>
2016-07-29 12:21:11 +0400

    UserService integration test in progress

---
+++ src/test/java/com/workfront/internship/event_management/service/UserServiceIntegrationTest.java
@@ -0,0 +1,59 @@
+package com.workfront.internship.event_management.service;
+
+import com.workfront.internship.event_management.TestHelper;
+import com.workfront.internship.event_management.exception.dao.DAOException;
+import com.workfront.internship.event_management.exception.dao.DuplicateEntryException;
+import com.workfront.internship.event_management.model.User;
+import org.junit.*;
+import org.mockito.Mockito;
+import org.mockito.internal.util.reflection.Whitebox;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Hermine Turshujyan 7/29/16.
+ */
+public class UserServiceIntegrationTest {
+
+    private static UserService userService;
+    private User testUser;
+
+    @BeforeClass
+    public static void setUpClass() throws DAOException {
+        userService = new UserServiceImpl();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        userService = null;
+    }
+
+    @Before
+    public void setUp() throws DAOException, DuplicateEntryException {
+        //create test user
+        testUser = TestHelper.createTestUser();
+
+    }
+
+    @After
+    public void tearDown() throws DAOException {
+        //delete inserted test users from db
+        userService.deleteAllUsers();
+
+        //delete test user object
+        testUser = null;
+    }
+
+    @Test
+    public void addAccount_() {
+        EmailService emailService = Mockito.mock(EmailServiceImpl.class);
+        Whitebox.setInternalState(userService, "emailService", emailService);
+        when(emailService.sendVerificationEmail(testUser)).thenReturn(true);
+
+        userService.addAccount(testUser);
+
+        User actualUser = userService.getUserById(testUser.getId());
+        assertNotNull(actualUser);
+    }
+}
