35e6865375399bbbb718ca3fead4ee243a532cd5
“Hermine Turshujyan” <hermineturshujyan@workfront.com>
2016-08-29 18:06:08 +0400

    Development and Test profile configurations created.

---
+++ src/main/java/com/workfront/internship/event_management/spring/DevApplicationConfig.java
@@ -0,0 +1,39 @@
+package com.workfront.internship.event_management.spring;
+
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by Hermine Turshujyan 8/29/16.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.workfront.internship.event_management")
+@Profile("Development")
+public class DevApplicationConfig {
+
+    @Bean
+    public DataSource getDevelopmentDataSource() throws IOException {
+
+        BasicDataSource dataSource = new BasicDataSource();
+
+        //loading db property list
+        Properties props = new Properties();
+        props.load(DevApplicationConfig.class.getClassLoader().getResourceAsStream("config.properties"));
+
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        dataSource.setUsername(props.getProperty("jdbc.username"));
+        dataSource.setPassword(props.getProperty("jdbc.password"));
+        dataSource.setUrl(props.getProperty("jdbc.url"));
+
+        return dataSource;
+    }
+
+}
---
+++ src/main/java/com/workfront/internship/event_management/spring/TestApplicationConfig.java
@@ -0,0 +1,38 @@
+package com.workfront.internship.event_management.spring;
+
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by Hermine Turshujyan 8/29/16.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.workfront.internship.event_management")
+@Profile("Test")
+public class TestApplicationConfig {
+    @Bean
+    public DataSource getTestDataSource() throws IOException {
+
+        BasicDataSource dataSource = new BasicDataSource();
+
+        //loading db property list
+        Properties props = new Properties();
+        props.load(ApplicationConfig.class.getClassLoader().getResourceAsStream("config.properties"));
+
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        dataSource.setUsername(props.getProperty("jdbc.username"));
+        dataSource.setPassword(props.getProperty("jdbc.password"));
+        dataSource.setUrl(props.getProperty("jdbc.url.test"));
+
+        return dataSource;
+    }
+
+}
