1843d313aca712244f23d6520713d8b3c6219aa3
“Hermine Turshujyan” <hermineturshujyan@workfront.com>
2016-08-31 12:12:45 +0400

    Date format change in event-edit.js

--- src/main/webapp/resources/js/event-edit.js
+++ src/main/webapp/resources/js/event-edit.js
@@ -3,10 +3,10 @@
  */
 $(document).ready(function () {
     $("#start_date").datepicker({
-        dateFormat: 'dd-mm-yy'
+        dateFormat: 'dd:mm:yyyy'
     });
     $("#end_date").datepicker({
-        dateFormat: 'dd-mm-yy'
+        dateFormat: 'dd:mm:yyyy'
     });
     $('#start_time').timepicker();
     $('#end_time').timepicker();
