be5f085478c28ad218aeb79edf8c4f24a8fdb26e
“Hermine Turshujyan” <hermineturshujyan@workfront.com>
2016-09-13 18:24:16 +0400

    Migrating from MYSQL to H2 db done.

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,7 @@
+INSERT INTO `event_category`
+VALUES (160, 'Category1', NULL, '2016-08-10 12:06:26'), (161, 'Category2', NULL, '2016-08-10 12:06:26'),
+  (162, 'Uncatigorized', NULL, '2016-08-10 12:06:31');
+
 INSERT INTO `event` VALUES
   (127, 'Event1', 'Event1 short description', 'Event1 full description', 'Yerevan, Armenia', NULL, NULL, NULL, NULL,
         160, 1, 0, '2016-08-10 12:09:26', '2016-08-10 12:09:26', '2016-08-10 12:09:26', NULL),
@@ -6,19 +10,9 @@ INSERT INTO `event` VALUES
   (129, 'Event3', 'Event3 short description', 'Event3 full description', 'Yerevan, Armenia', NULL, NULL, NULL, NULL,
         160, 1, 0, '2016-08-31 17:27:25', '2016-08-10 12:10:10', '2016-08-10 12:10:10', NULL);
 
-INSERT INTO `event_category`
-VALUES (160, 'Category1', NULL, '2016-08-10 12:06:26'), (161, 'Category2', NULL, '2016-08-10 12:06:26'),
-  (162, 'Uncatigorized', NULL, '2016-08-10 12:06:31');
-
---
---
-
 INSERT INTO `user` VALUES
   (355, 'Hermine', 'Turshujyan', '7962468883c36dc5666c3aaa3bb3d38d0900ec827c203dd53d86babf0c3094e6',
    'turshujyan@gmail.com', '', NULL, 0, '2016-08-26 12:49:15');
-
---
---
 
 
 INSERT INTO `user_response` VALUES (3, 'Maybe'), (2, 'No'), (4, 'Undefined'), (1, 'Yes');
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE event (
-  id              INTEGER      NOT NULL,
-  title           VARCHAR(255) NOT NULL,
+  id              INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title           VARCHAR(255)     NOT NULL,
   short_desc      VARCHAR(300),
   full_desc       VARCHAR(1500),
   location        VARCHAR(200),
@@ -8,81 +8,133 @@ CREATE TABLE event (
   lng             DOUBLE,
   file_path       VARCHAR(255),
   image_path      VARCHAR(255),
-  category_id     INTEGER      NOT NULL,
-  public_accessed INTEGER      NOT NULL,
-  guests_allowed  INTEGER      NOT NULL,
-  start           TIMESTAMP    NOT NULL,
-  end             TIMESTAMP    NOT NULL,
-  creation_date   TIMESTAMP    NOT NULL,
+  category_id     INTEGER          NOT NULL,
+  public_accessed INTEGER          NOT NULL,
+  guests_allowed  INTEGER          NOT NULL,
+  start           TIMESTAMP        NOT NULL,
+  end             TIMESTAMP        NOT NULL,
+  creation_date   TIMESTAMP        NOT NULL,
   last_modified   TIMESTAMP
 );
 
+
 CREATE TABLE event_category (
-  id            INTEGER      NOT NULL,
-  title         VARCHAR(100) NOT NULL,
+  id            INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title         VARCHAR(100)     NOT NULL,
   description   VARCHAR(150),
   creation_date TIMESTAMP
 );
 
 CREATE TABLE event_invitation (
-  id              INTEGER     NOT NULL,
-  event_id        INTEGER     NOT NULL,
-  user_id         INTEGER     NOT NULL,
-  user_role       VARCHAR(20) NOT NULL,
-  user_response   INTEGER     NOT NULL,
-  attendees_count INTEGER,
-  participated    INTEGER,
-  last_modified   TIMESTAMP,
-  creation_date   TIMESTAMP
+  id               INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  event_id         INTEGER          NOT NULL,
+  user_id          INTEGER          NOT NULL,
+  user_role        VARCHAR(20)      NOT NULL,
+  user_response_id INTEGER          NOT NULL,
+  attendees_count  INTEGER,
+  participated     INTEGER,
+  last_modified    TIMESTAMP,
+  creation_date    TIMESTAMP
 );
 
 CREATE TABLE event_media (
-  id            INTEGER      NOT NULL,
-  event_id      INTEGER      NOT NULL,
-  path          VARCHAR(300) NOT NULL,
-  media_type_id INTEGER      NOT NULL,
+  id            INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  event_id      INTEGER          NOT NULL,
+  path          VARCHAR(300)     NOT NULL,
+  media_type_id INTEGER          NOT NULL,
   description   VARCHAR(300),
-  uploader_id   INTEGER      NOT NULL,
-  upload_date   TIMESTAMP    NOT NULL
+  uploader_id   INTEGER          NOT NULL,
+  upload_date   TIMESTAMP        NOT NULL
 );
 
 CREATE TABLE event_recurrence (
-  id                   INTEGER NOT NULL,
-  event_id             INTEGER NOT NULL,
-  recurrence_type_id   INTEGER NOT NULL,
-  repeat_interval      INTEGER NOT NULL,
+  id                   INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  event_id             INTEGER          NOT NULL,
+  recurrence_type_id   INTEGER          NOT NULL,
+  repeat_interval      INTEGER          NOT NULL,
   recurrence_option_id INTEGER,
   repeat_end           TIMESTAMP
 );
 
 CREATE TABLE media_type (
-  id    INTEGER     NOT NULL,
-  title VARCHAR(45) NOT NULL
+  id    INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(45)      NOT NULL
 );
 
 CREATE TABLE recurrence_option (
-  id                 INTEGER      NOT NULL,
-  recurrence_type_id INTEGER      NOT NULL,
-  title              VARCHAR(200) NOT NULL,
+  id                 INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  recurrence_type_id INTEGER          NOT NULL,
+  title              VARCHAR(200)     NOT NULL,
   abbreviation       VARCHAR(20)
 );
+
 CREATE TABLE recurrence_type (
-  id            INTEGER      NOT NULL,
-  title         VARCHAR(200) NOT NULL,
-  interval_unit VARCHAR(45)  NOT NULL
+  id            INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title         VARCHAR(200)     NOT NULL,
+  interval_unit VARCHAR(45)      NOT NULL
 );
+
 CREATE TABLE user (
-  id                INTEGER      NOT NULL,
-  first_name        VARCHAR(300) NOT NULL,
-  last_name         VARCHAR(300) NOT NULL,
-  password          VARCHAR(150) NOT NULL,
-  email             VARCHAR(100) NOT NULL,
+  id                INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  first_name        VARCHAR(300)     NOT NULL,
+  last_name         VARCHAR(300)     NOT NULL,
+  password          VARCHAR(150)     NOT NULL,
+  email             VARCHAR(100)     NOT NULL,
   phone_number      VARCHAR(30),
   avatar_path       VARCHAR(300),
   verified          INTEGER,
   registration_date TIMESTAMP
 );
+
 CREATE TABLE user_response (
-  id    INTEGER     NOT NULL,
-  title VARCHAR(45) NOT NULL
+  id    INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(45)      NOT NULL
 );
+
+ALTER TABLE event
+  ADD FOREIGN KEY (category_id) REFERENCES event_category (id)
+  ON DELETE SET NULL
+  ON UPDATE NO ACTION;
+
+ALTER TABLE event_invitation
+  ADD FOREIGN KEY (event_id) REFERENCES event (id)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+ALTER TABLE event_invitation
+  ADD FOREIGN KEY (user_id) REFERENCES user (id)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+ALTER TABLE event_invitation
+  ADD FOREIGN KEY (user_response_id) REFERENCES user_response (id)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE event_media
+  ADD FOREIGN KEY (event_id) REFERENCES event (id);
+ALTER TABLE event_media
+  ADD FOREIGN KEY (media_type_id) REFERENCES media_type (id)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+ALTER TABLE event_media
+  ADD FOREIGN KEY (uploader_id) REFERENCES user (id)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE event_recurrence
+  ADD FOREIGN KEY (event_id) REFERENCES event (id)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+ALTER TABLE event_recurrence
+  ADD FOREIGN KEY (recurrence_option_id) REFERENCES recurrence_option (id)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+ALTER TABLE event_recurrence
+  ADD FOREIGN KEY (recurrence_type_id) REFERENCES recurrence_type (id)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE recurrence_option
+  ADD FOREIGN KEY (recurrence_type_id) REFERENCES recurrence_type (id)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
